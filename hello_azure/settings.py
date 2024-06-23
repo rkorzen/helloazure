15b1c9162cf8d758c71a09bcb439ca9e7254e601
rkorzen <korzeniewski@gmail.com>
2024-06-23 22:40:48 +0200

    update wsgi

--- hello_azure/settings.py
+++ hello_azure/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alxazure.azurewebsites.net"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 if 'CODESPACE_NAME' in os.environ:
     CSRF_TRUSTED_ORIGINS = [f'https://{os.getenv("CODESPACE_NAME")}-8000.{os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")}']
