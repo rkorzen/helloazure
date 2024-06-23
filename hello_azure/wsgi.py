f111b59c5e41cf9da38db3bfed0b57dac91188ef
rkorzen <korzeniewski@gmail.com>
2024-06-23 22:46:41 +0200

    update wsgi

--- hello_azure/wsgi.py
+++ hello_azure/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 settings_module = 'hello_azure.production' if 'WEBSITE_HOSTNAME' in os.environ else 'hello_azure.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_azure.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
