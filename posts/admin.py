8c9400a9ab28da7446d396968054b207f0b3b100
rkorzen <korzeniewski@gmail.com>
2024-06-24 13:19:21 +0200

    add post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+
+admin.site.register(Post, PostAdmin)
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def posts_list(request):
-    return render(request, 'posts/posts_list.html', {})
+    return render(request, 'posts/posts_list.html', {"posts": Post.objects.all()})
 
-def post_details(request):
-    return render(request, 'posts/post_details.html', {})
+def post_details(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'posts/post_details.html', {"post": post})
